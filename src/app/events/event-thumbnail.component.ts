390515ec2fbaef4b90302c7178ca7eededd91f77
Carlos Carrero <carlos.carrero@gmail.com>
2019-06-29 21:51:50 -0500

    Application initial structure

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent {
+
+  @Input() event: any;
+
+  getStartTimeClass(): any {
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
+  }
+}
