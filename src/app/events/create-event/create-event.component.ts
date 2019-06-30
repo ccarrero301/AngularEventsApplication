91c1a6427f323fca98d8d381c046a7ecbd50e761
Carlos Carrero <carlos.carrero@gmail.com>
2019-06-30 08:35:58 -0500

    Removing unused selector

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'selector-name',
   templateUrl: './create-event.component.html'
 })
 
