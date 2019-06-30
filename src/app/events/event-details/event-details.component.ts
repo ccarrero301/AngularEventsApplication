b6576530b45e446f08468e370eda918d8e8749a9
Carlos Carrero <carlos.carrero@gmail.com>
2019-06-30 08:18:19 -0500

    Getting route parameter from url

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -8,9 +9,13 @@ import { EventService } from '../shared/event.service';
 export class EventDetailsComponent implements OnInit {
   event: any;
 
-  constructor(private eventService: EventService) { }
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(1);
+    this.route.params.subscribe(route => {
+      this.event = this.eventService.getEvent(+route['id']);
+    });
   }
 }
