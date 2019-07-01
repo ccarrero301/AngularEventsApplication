3252b84ded0c58f334fd52e41f165c0cb9877483
Carlos Carrero <carlos.carrero@gmail.com>
2019-07-01 14:45:24 -0500

    Upgrading the modal to bind to an id

--- src/app/common/modal-trigger.directive.ts
+++ src/app/common/modal-trigger.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, OnInit, Inject, ElementRef } from '@angular/core';
+import { Directive, OnInit, Inject, ElementRef, Input } from '@angular/core';
 import { JQ_TOKEN } from './jQuery.service';
 
 @Directive({ selector: '[modal-trigger]' })
 export class ModalTriggerDirective implements OnInit {
 
   private htmlElement: HTMLElement;
+
+  @Input('modal-trigger') modalId: string;
 
   constructor(
     @Inject(JQ_TOKEN) private $: any,
@@ -14,7 +16,7 @@ export class ModalTriggerDirective implements OnInit {
 
   ngOnInit(): void {
     this.htmlElement.addEventListener('click', e => {
-      this.$('#simple-modal').modal({});
+      this.$(`#${this.modalId}`).modal({});
     });
   }
 
